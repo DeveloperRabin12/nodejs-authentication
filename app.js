f0378c813bb42eab5ea0919469891a88e6137f16
Rabin Gapi <74033795+DeveloperRabin12@users.noreply.github.com>
2023-08-15 22:32:14 +0545

    update

--- app.js
+++ app.js
@@ -1,36 +1,126 @@
-const express= require("express");
-const {users} = ("/model/");
-//const bcrypt = require("bcryptjs");
-const app = express();
+const express = require('express');  //express laii require gareko
+const { users } = require('./model/index');
+const bcrypt = require("bcryptjs");
+
+const app =express()      // require laii call gareko
 
 
-require("./model/")
+//require database
+require("./model/index")
 
-app.set("view engine","ejs")
+//setting up ejs, telling nodejs to use ejs
+app.set('view engine', 'ejs')
+
+// Request(req)/Response(res) cycle
+app.get('/',(req, res) =>{
+    res.render('home.ejs');
+});
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
 
-app.get("/",(req,res)=>{
-    res.send("you are in homepage");
-})
 
+//register
+app.get('/register',(req, res) =>{
+    res.render('register');
+});
 
-app.get("/register",(req,res)=>{
-    res.render("register.ejs");
-})
+//setting
+app.post('/createUser', async(req, res ) =>{
+    console.log(req.body);
 
-//post api
-app.post("/createUser",async (req, res)=>{
+    //database ma halnu paryo
     await users.create({
-        username: req.body.username,
-        email: req.body.email,
-        password: req.body.password, 
-    });
-    res.send("user created successfully");
+        username : req.body.username,
+        email : req.body.email,
+        password : bcrypt.hashSync(req.body.password,10),
+    })
+
+    // res.send("Successful")
+    res.redirect('/login')
 })
 
-app.listen(4500,()=>{
-    console.log("registration has satrted");
+
+//Login
+app.get('/login',(req, res) =>{
+    res.render('login');
+});
+
+// app.post('/createLogin', async(req, res ) =>{
+//     console.log(req.body);
+
+//         const email =  req.body.email
+//         const password = req.body.password
+
+//          const userFound = await users.findAll({
+//             where:{
+//                 email: email,
+//                 // password:password,
+//             },   
+//         });
+        
+//         // console.log(userFound);
+
+//         if(userFound.length == 0){
+//             res.send('Invaild Username or Password')
+//         }else{
+//             const databasePassword = userFound[0].password;
+//             console.log(databasePassword)
+//             console.log(password)
+
+
+
+//             const isPasswordCorrect = bcrypt.compareSync(password,databasePassword);
+//             console.log(isPasswordCorrect);
+
+
+//             if (isPasswordCorrect){
+//                 res.send("Login Successfully")
+//             }else{
+//                 res.send("Invaild password");
+//             }
+//         }
+// });
+
+
+// LOGIN user post API
+app.post("/createLogin", async (req, res) => {
+    // email , password
+    const email = req.body.email;
+    const password = req.body.password;
+  
+    //aako email registered xa ki xainw check garnu paryo
+    const userFound = await users.findAll({
+      where: {
+        email: email,
+      },
+    });
+  
+    // if registered xainw vaney(no)
+    if (userFound.length == 0) {
+      // error faldinu paryo invalid email or email not registered error
+      res.send("Invalid email or password");
+    } else {
+      const databasePassword = userFound[0].password; // database pahila register garda ko password
+      //if registered xa vaney (yes)
+  
+      // if yes(xa) vaney ,password check garnu paryo
+      const isPasswordCorrect = bcrypt.compareSync(password, databasePassword);
+  
+      if (isPasswordCorrect) {
+        // match vayo(yes),login sucessfully
+        res.send("Login Sucessfull");
+      } else {
+        // match vayena (no) , error->invalid password
+        res.send("Invalid email or password");
+      }
+    }
+  });
+
+
+
+//port no : 1300-65000, 1300 vanda tala chai internet system le use garirahunchha
+app.listen(3000, function(){
+    console.log("Node js project has started at port 3000");
 })
